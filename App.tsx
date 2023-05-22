cb1b7a21c9092ea580a0b47921076ff9d859c3c1
hernan0066 <hernan0066@gmail.com>
2023-05-22 14:00:57 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { RoundedButton } from "./src/Presentation/components/RondedButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/logo.jpeg")} style={styles.imageBack} />
+      <View style={styles.loginForm}>
+        <Text style={styles.title}>Ingresar</Text>
+        <View style={styles.textInputContainer}>
+          <Image
+            source={require("./assets/user.png")}
+            style={styles.iconImage}
+          />
+          <TextInput style={styles.textInput} placeholder="Ingresa tu correo" />
+        </View>
+        <View style={styles.textInputContainer}>
+          <Image
+            source={require("./assets/password.png")}
+            style={styles.iconImage}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Ingresa tu contraseña"
+          />
+        </View>
+        <RoundedButton
+          text={"ENVIAR"}
+          onPress={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +39,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff4136",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+  imageBack: {
+    width: "100%",
+    height: "70%",
+    position: "relative",
+    top: -130,
+  },
+  loginForm: {
+    position: "absolute",
+    width: "100%",
+    height: "38%",
+    backgroundColor: "#fff",
+    bottom: 0,
+    borderTopStartRadius: 40,
+    borderTopEndRadius: 40,
+    padding: 20,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 25,
+  },
+  textInput: {
+    flex: 1,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    padding: 5,
+    marginBottom: 25,
+  },
+  textInputContainer: {
+    flexDirection: "row",
+    gap: 7,
+  },
+  iconImage: {
+    width: 30,
+    height: 30,
+    marginTop: 5,
   },
 });
